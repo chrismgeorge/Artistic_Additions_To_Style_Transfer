a7668f932ae718c8ec4c9c5e0117394749bea20e
Christopher George <chris@Christophers-MacBook-Pro.local>
2018-10-18 17:53:05 -0400

    added all pics to original pics read me

--- general_stuff.py
+++ general_stuff.py
@@ -1,7 +1,18 @@
 import os
 
-directory_path = "./mom/"
-i = 0
+# directory_path = "./mom/"
+# i = 0
+# for filename in os.listdir(directory_path):
+#     os.rename(directory_path+filename, directory_path+str(i)+".jpg")
+#     i += 1
+
+
+directory_path = "./regular_photos/"
+text = ""
 for filename in os.listdir(directory_path):
-    os.rename(directory_path+filename, directory_path+str(i)+".jpg")
-    i += 1
+    if (".jpg" in filename):
+        text += '<p align="center">  <img width="100" height="100" src="'+filename+'?"> </p>'
+        text += '<p align="center">'+filename+'</p>'
+        text += "\n"
+
+print(text)
